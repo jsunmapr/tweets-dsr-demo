b74f4c91c5c7da9becc22c8e8264cea41232116f
james sun <jsun@mapr.com>
2018-01-08 10:22:43 -0800

    demo

--- tweets.scala
+++ tweets.scala
@@ -1,11 +1,4 @@
 import com.mapr.db.spark._
 val tweets=sc.loadFromMapRDB("/tmp/tweets")
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.types.{StructType,StructField,StringType};
-val sqlContext = new org.apache.spark.sql.SQLContext(sc)
-val schema = StructType( List( StructField("text", StringType, true), StructField("screen_name", StringType, true) ))
-val df = sqlContext.createDataFrame(tweets.map(tt => tt.text[String] +"\t"+ tt.screen_name[String]).map(tt => Row(tt.split("\t")(0), tt.split("\t")(1))), schema)
-
-df.createOrReplaceTempView("tTBL")
-val results=sqlContext.sql("select screen_name,text from tTBL").show()
-quit
+tweets.count
+tweets.select("hashtags","text").take(5)
