1610dea8687058e63a2932d7b0cab9b04c7235cc
james sun <jsun@mapr.com>
2018-01-08 10:04:59 -0800

    demo

--- tweets.scala
+++ tweets.scala
@@ -6,6 +6,6 @@ val sqlContext = new org.apache.spark.sql.SQLContext(sc)
 val schema = StructType( List( StructField("text", StringType, true), StructField("screen_name", StringType, true) ))
 val df = sqlContext.createDataFrame(tweets.map(tt => tt.text[String] +"\t"+ tt.screen_name[String]).map(tt => Row(tt.split("\t")(0), tt.split("\t")(1))), schema)
 
-df.registerTempTable("tTBL")
+df.createOrReplaceTempView("tTBL")
 val results=sqlContext.sql("select screen_name,text from tTBL").show()
-:q
+quit
