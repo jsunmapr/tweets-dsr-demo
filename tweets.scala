bb66133a2b0dd20b8062f901d8a55fa5d42a14cf
james sun <jsun@mapr.com>
2018-01-08 10:29:09 -0800

    demo

--- tweets.scala
+++ tweets.scala
@@ -1,4 +1,3 @@
 import com.mapr.db.spark._
 val tweets=sc.loadFromMapRDB("/tmp/tweets")
-tweets.count
-tweets.select("screen_name","text").take(5).foreach(println(_))
+tweets.select("screen_name","text").take(5).mkString("\n")
